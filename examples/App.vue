87ce6e37ef292a303ecafe503a27de1988f6b07e
king <pengkaiwang@canway.net>
2022-11-23 17:10:10 +0800

    feat: 完成devops-tree主体功能

--- examples/App.vue
+++ examples/App.vue
@@ -1,27 +1,97 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <demo-a />
+
+    <devops-tree
+      show-checkbox
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from '../src/components/tree'
+import DevopsTree from "../src/devops-tree.vue";
+import demoA from '../src/demo.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    DevopsTree,
+    demoA
+  },
+  data() {
+    return {
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1",
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1",
+                }
+              ]
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1",
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1",
+                }
+              ]
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1",
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/demo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eos ad illum animi! Ad, nesciunt explicabo illo enim illum modi unde minus corrupti numquam suscipit laudantium excepturi natus rem voluptatibus?
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
